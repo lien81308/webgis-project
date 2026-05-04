9d0d191947429bef4e419facf64de4e0e6ef12d3
lien81308 <148791433+lien81308@users.noreply.github.com>
2026-05-04 13:48:44 +0800

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const mapThemes = {
     light: 'mapbox://styles/mapbox/light-v10'
 };
 
-let currentTheme = 'dark'; // 預設使用深色主題'white'('dark')
+let currentTheme = 'light'; // 預設使用深色主題'white'('dark')
 
 // 行政區中心點
 const districtCenters = {
